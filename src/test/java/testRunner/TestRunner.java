765885d4d5dddfc8a919c5a27395e4e2f700d74c
vahinig03 <vahini.r44@gmail.com>
2025-07-03 21:10:48 -0400

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,64 +1,43 @@
 package testRunner;
 
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import listeners.ITestListenerImpl;
+import utilities.ConfigReader;
 
-import io.cucumber.junit.CucumberOptions;
+@Listeners(ITestListenerImpl.class)
+@CucumberOptions(		//Cucumber Test Runner using TestNG
 
+		plugin = {"pretty","html:target/dsalgo.html","html:target/cucumber-reports/reports.html",
+				 "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/extent-report.html",// Generates readable and HTML reports
+		          "rerun:target/failedrerun.txt",
+		          "com.aventstack.chaintest.plugins.ChainTestCucumberListener:"},
+		         monochrome=false, 
+		features = {//"src/test/resources/features/"},
+		glue={"stepdefinition","hooks"})// Packages containing step definitions and hooks
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		features = {"src/test/resources/Feature/PremiumUser_Homepage.feature"},
-
-    glue = {"stepDefinitions", "hooks"},
-    dryRun = false,
-    monochrome = true,
-    plugin = {
-    		"pretty","html:target/SweetBalanceApplication.html",
-			  "html:target/cucumber-reports/reports.html",
-			  "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-	          "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/extent-report.html",// Generates readable and HTML reports
-	          "rerun:target/failedrerun.txt",
-	          "com.aventstack.chaintest.plugins.ChainTestCucumberListener:"})
-	        
-    
-
-public class TestRunner {
-    // No need to extend anything or add @DataProvider
+public class TestRunner extends AbstractTestNGCucumberTests{	
+	String browser;
+	@Override
+	@DataProvider(parallel = false) // Set to true for parallel scenario execution and set false for crossbrowser
+	public Object[][] scenarios(){
+		return super.scenarios();
+	}	
+	
+	@BeforeClass    
+	@Parameters("browser")	
+	public void defineBrowser(@Optional String browser) throws Throwable {
+		this.browser = browser;
+		if(browser != null && !browser.equals("param-val-not-found")) {
+			ConfigReader.setBrowserType(browser);
+		}
+	}
 }
-
-
-
-//@CucumberOptions(//Cucumber Test Runner using TestNG
-//
-//		plugin = {"pretty","html:target/SweetBalanceApplication.html",
-//				  "html:target/cucumber-reports/reports.html",
-//				  "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-//		          "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:target/extent-report.html",// Generates readable and HTML reports
-//		          "rerun:target/failedrerun.txt",
-//		          "com.aventstack.chaintest.plugins.ChainTestCucumberListener:"},
-//		         monochrome=false, // If true, enables colored console output (more readable)
-//		features = {"/src/test/resources/features/PremiumUser_Homepage.feature"}, // Path to the feature files
-// 		glue={"stepdefinition","hooks"})// Packages containing step definitions and hooks
-//
-//  public class TestRunner {
-////  extends AbstractTestNGCucumberTests{	
-////	String browser;
-////	@Override
-////	@DataProvider(parallel = false) // Set to true for parallel scenario execution and set false for crossbrowser
-////	public Object[][] scenarios(){
-////		return super.scenarios();
-////	}	
-////	
-////	@BeforeClass    //Enable during cross browser testing
-////	@Parameters("browser")	//Inject browser value from testng-parallel.xml into this method for cross browser execution
-////	public void defineBrowser(@Optional String browser) throws Throwable {
-////		this.browser = browser;
-////		if(browser != null && !browser.equals("param-val-not-found")) {
-////			ConfigReader.setBrowserType(browser);
-////		}
-////	}
-//}
